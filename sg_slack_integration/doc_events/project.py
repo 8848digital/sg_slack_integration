05d563592f577c4ef7e457829794ed0f6c308191
rahulgupta8848 <rahul.gupta@8848digital.com>
2024-01-09 18:28:29 +0530

    set topic and set description according to the opportunity and project

--- sg_slack_integration/doc_events/project.py
+++ sg_slack_integration/doc_events/project.py
@@ -1,34 +1,15 @@
 import frappe
 import requests
 import json
-from sg_slack_integration.doc_events.ped import set_topic,set_description
+from sg_slack_integration.doc_events.ped import set_topic,set_description, get_channel_id
 
 
 def validate(self, method=None):
-    topic_and_description = frappe.get_value("Project",self.opportunity,['project_name', 'customer'])
+    topic_and_description = frappe.get_value("Project",self.name,['project_name', 'customer'])
     create_slack_channel(self)
     channel = get_channel_id(self)
     set_topic(self,channel, topic_and_description.project_name)
     set_description(self,channel, topic_and_description.customer)
-    
-def get_channel_id(self):
-    if self.doctype == "Project":
-        channel_name = self.name.lower().replace(' ', '_')
-    if self.doctype == "Project Employee Distribution":
-        channel_name = self.opportunity.lower()
-    token = frappe.db.get_single_value('Token', 'token')
-    url = "https://slack.com/api/conversations.list"	
-    headers = {	
-		'Authorization': f'Bearer {token}',
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}   
-    payload = {"limit": 999}
-    response = requests.request("POST",url, headers=headers, data=payload)
-    res = response.json()
-    if res['ok']:
-        for channel in res['channels']:
-            if channel.get('name') == channel_name:
-                return channel.get('id')
      
 def create_slack_channel(self,method=None):
     try:
