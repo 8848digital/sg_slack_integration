54ebd1036b6b3666a4e5fb997203be62078333ff
rahulgupta8848 <rahul.gupta@8848digital.com>
2024-01-24 10:52:03 +0530

    fix: channel does not exists msg print during opportunity workflow

--- sg_slack_integration/doc_events/opportunity.py
+++ sg_slack_integration/doc_events/opportunity.py
@@ -1,11 +1,8 @@
-import frappe
-import requests
+# import frappe
 from sg_slack_integration.doc_events.common_function import archive_channel,get_channel_id
 
 def validate(self,method=None):
-    channel = get_channel_id(self) 
+    channel = get_channel_id(self)
     if channel:
         if self.workflow_state == "Client Approval Pending":
             archive_channel(self,channel)
-    else:
-        frappe.msgprint("Channel does not existes")
