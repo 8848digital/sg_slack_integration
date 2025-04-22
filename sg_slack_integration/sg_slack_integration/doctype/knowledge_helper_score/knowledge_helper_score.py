fc64c0baa96c6b7039fd2f5045f2e1fcc5f5911d
saffaat.gazi <saffaat.gazi@elastic.run>
2025-04-22 15:30:55 +0530

    feat: slack knowledge helper changes

--- sg_slack_integration/sg_slack_integration/doctype/knowledge_helper_score/knowledge_helper_score.py
+++ sg_slack_integration/sg_slack_integration/doctype/knowledge_helper_score/knowledge_helper_score.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2025, 8848 Digital and contributors
 # For license information, please see license.txt
 
+# from sg_slack_integration.sg_slack_integration.doctype.knowledge_helper_score.knowledge_helper_score import slack_event
 import frappe
 from frappe.model.document import Document
 import json
@@ -10,11 +11,16 @@ from frappe.utils import getdate, nowdate
 class KnowledgeHelperScore(Document):
 	pass
 
-# from sg_slack_integration.sg_slack_integration.doctype.knowledge_helper_score.knowledge_helper_score import slack_event
+
 
 
 @frappe.whitelist(allow_guest=True)
 def slack_event():
+	settings = frappe.get_cached_doc("Slack Integration Settings")
+	if not settings.enable_scoring:
+		frappe.log_error("Knowledge Helper Scoring",
+		                 "Scoring not Enabled in Slack Integration Settings")
+		return
 	data = json.loads(frappe.request.data)
 	frappe.log_error("data", data)
 
@@ -27,37 +33,47 @@ def slack_event():
 	channel_id = event.get("item", {}).get("channel")
 	message_ts = event.get("item", {}).get("ts")
 	reactor = event.get("user")  # Who reacted
+	main_message_ts = ""
 
-	if reaction == "white_check_mark":
-		# Fetch the replied message details
-		message_author, thread_ts = fetch_message_author(channel_id, message_ts)
+	if channel_id == settings.channel_id_to_calculate_the_score_from:
+		if reaction == settings.score_based_on_emoji_reaction:
+			# Fetch the replied message details
+			message_author, thread_ts = fetch_message_author(channel_id, message_ts)
 
-		# Fetch the parent message (thread starter)
-		thread_author, _ = fetch_message_author(channel_id, thread_ts)
+			# Fetch the parent message (thread starter)
+			thread_author, main_message_ts = fetch_message_author(channel_id, thread_ts)
 
-		# Check if the reactor is the original asker
-		if reactor == thread_author:
-			# ✅ Valid marking - credit the message_author (helper)
-			helper_info = fetch_user_info(message_author)  # reply author info
-			marker_info = fetch_user_info(reactor)         # person who marked as solved
+			# Check if the reactor is the original asker
+			# if reactor == thread_author:
+			# 	# ✅ Valid marking - credit the message_author (helper)
 
-			frappe.get_doc({
-				"doctype": "Knowledge Helper Score",
-				"channel_id": channel_id,
-				"thread_ts": thread_ts,
-				"message_ts": message_ts,
-				"helper_user_id": helper_info['user_id'],
-				"helper_email": helper_info['email'],
-				# "helper_name": helper_info['real_name'],
-				"author_user_id": marker_info['user_id'],
-				"author_email": marker_info['email'],
-				# "marked_by_name": marker_info['real_name'],
-				"reaction": reaction,
-				"date": nowdate()
-			}).insert(ignore_permissions=True)
-		else:
-			# 🚫 Someone else reacted, ignore
-			pass
+			# reply author info
+			helper_info = fetch_user_info(message_author)
+			# query owner
+			thread_author_info = fetch_user_info(thread_author)
+			# person who marked as solved
+			reactor_info = fetch_user_info(reactor)
+
+			if not frappe.db.exists("Knowledge Helper Score", {"main_message_ts": main_message_ts, "author_email_id": thread_author_info['email']}):
+				frappe.get_doc({
+					"doctype": "Knowledge Helper Score",
+					"channel_id": channel_id,
+					"thread_ts": thread_ts,
+					"message_ts": message_ts,
+					"helper_user_id": helper_info['user_id'],
+					"helper_email": helper_info['email'],
+					# "helper_name": helper_info['real_name'],
+					"author_user_id": thread_author_info['user_id'],
+					"author_email": thread_author_info['email'],
+					"verified_by_user_id": reactor_info['user_id'],
+					"verified_by_email": reactor_info['email'],
+					# "marked_by_name": marker_info['real_name'],
+					"reaction": reaction,
+					"date": nowdate()
+				}).insert(ignore_permissions=True)
+			# else:
+			# 	# 🚫 Someone else reacted, ignore
+			# 	pass
 
 
 def fetch_message_author(channel_id, message_ts):
@@ -69,7 +85,7 @@ def fetch_message_author(channel_id, message_ts):
 	slack_token = frappe.db.get_single_value(
 		"Slack Integration Settings", "bday_bot_token")
 	if not slack_token:
-		frappe.throw("Slack Bot Token not configured in site config!")
+		frappe.throw("Slack Bot Token not configured in Slack Integration Settings!")
 
 	url = "https://slack.com/api/conversations.replies"
 
@@ -87,12 +103,14 @@ def fetch_message_author(channel_id, message_ts):
 	data = response.json()
 
 	if not data.get("ok"):
-		frappe.throw(f"Error fetching message: {data.get('error')}")
+		frappe.log_error("Knowledge Helper Score | fetch_message_author",
+		                 f"Error fetching message: {data.get('error')}")
 
 	messages = data.get("messages", [])
 
 	if not messages:
-		frappe.throw("No messages found for given timestamp.")
+		frappe.log_error("Knowledge Helper Score | fetch_message_author",
+		                 "No messages found for given timestamp.")
 
 	message = messages[0]
 
@@ -112,7 +130,7 @@ def fetch_user_info(user_id):
 
 	# slack_token = frappe.conf.get("slack_bot_token")
 	if not slack_token:
-		frappe.throw("Slack Bot Token not configured in site config!")
+		frappe.throw("Slack Bot Token not configured in Slack Integration Settings!")
 
 	url = "https://slack.com/api/users.info"
 
@@ -128,7 +146,8 @@ def fetch_user_info(user_id):
 	data = response.json()
 
 	if not data.get("ok"):
-		frappe.throw(f"Error fetching user info: {data.get('error')}")
+		frappe.log_error("Knowledge Helper Score | fetch_user_info",
+		                 f"Error fetching user info: {data.get('error')}")
 
 	user = data.get("user", {})
 	email = user.get("profile", {}).get("email", "")
