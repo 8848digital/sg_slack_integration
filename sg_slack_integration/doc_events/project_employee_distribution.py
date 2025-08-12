a44bb26436411328c57e2ab9b3e0e8204fdf6768
kanchan-8848digital <kanchan@8848digital.com>
2025-08-12 14:39:42 +0530

    feat: reminder

--- sg_slack_integration/doc_events/project_employee_distribution.py
+++ sg_slack_integration/doc_events/project_employee_distribution.py
@@ -5,7 +5,8 @@ import requests
 from sg_slack_integration.doc_events.poll_api import *
 from frappe.model.workflow import apply_workflow
 from frappe.core.doctype.version.version import get_diff
-
+from strategic_gears.strategic_gears.utils.mail import get_users_from_email_group
+from frappe.utils import getdate
 def after_insert(self,method):
     if self.ped_from=='Opportunity':
         employee_details=self.distribution_detail
@@ -139,11 +140,16 @@ def handle_poll_response():
                     frappe.set_user(approver)
                     for ped_emp in ped.get('distribution_detail'):
                         if ped_emp.get('name')==poll_id and not ped_emp.get('invite_accepted') and not ped_emp.get('invite_rejected'):
+                            email_template=''
                             if selected_option=='Yes':
                                 ped_emp.update({'invite_accepted':1})
+                                email_template = frappe.db.get_single_value(
+                                    "Slack Integration Settings", 'accepted_notification')
                             else:
                                 ped_emp.update({'invite_rejected':selected_option})
                                 ped.append('users_rejected',{'employee':distribution_details.get('employee'),'employee_name':distribution_details.get('employee_name')})
+                                email_template = frappe.db.get_single_value(
+                                    "Slack Integration Settings", 'rejected_notification')
                             ped.save(ignore_permissions=True)
                             send_ephemeral_message(
                                 slack_token, channel_id, user_id, ts, selected_option, slack_data.get("message", {}).get("blocks", ""), block_id, poll_id
@@ -152,10 +158,66 @@ def handle_poll_response():
                             poll_message = f"Response Received by Employee - {user_id} in {poll_id}\n - {selected_option}"
                             create_slack_log_for_poll(self=distribution_details, status="Success",
                                                                 poll_type="Receive Response", poll_result=poll_message)
+                            if email_template:
+                                sending_response_mail(email_template,ped_doc=distribution_details.get('parent'),ped_child_table=distribution_details)
+
 
             return {"text": f"Response Recorded for '{selected_option}' recorded."}
     except Exception as e:
         create_slack_log_for_poll(self=doc, status="Error",
 		                          poll_type="Receive Response", error=str(frappe.get_traceback(e)))
         frappe.log_error("Error in slack", frappe.get_traceback(e))
+
+def sending_response_mail(email_template,ped_doc,ped_child_table):
+    doc=frappe.get_doc('Project Employee Distribution',ped_doc)
+    email_group = frappe.db.get_single_value(
+                                    "Slack Integration Settings", 'email_group')
+    if email_group:
+        email_group_users=get_users_from_email_group(email_group)
+        email_template_doc=frappe.get_doc('Email Template',email_template)
+        subject=email_template_doc.get('subject')
+        doc.update({'employee_name':ped_child_table.get('employee_name'),'from_date_employee':ped_child_table.get('from_date'),'to_date_employee':ped_child_table.get('to_date')})
+        content = frappe.render_template(email_template_doc.response_html, {"doc": doc})
+        frappe.sendmail(recipients=email_group_users,
+                 subject=subject,
+                 content=content,
+                 reference_doctype=doc.doctype, 
+                 reference_name=doc.name)
+
+@frappe.whitelist()
+def send_action_reminder():
+    today_date=getdate()
+    PED=frappe.get_all('Project Employee Distribution',{'ped_from':'Opportunity'})
+    ped_tables=frappe.get_all('Project Employee Distribution Detail',{'invite_sent':1,'invite_accepted':0,'invite_rejected':['is','not set'],'parent':['in',PED]},['*'])
+    users_list=[]
+    # for ped_forms in ped_tables:
+    #     if getdate(ped_forms.get('creation'))!=today_date:
+    #         users_list.append(ped_forms.get('employee_user_id'))
+    users_list=['saffaat@8848digital.com']
+    send_reminder_on_slack(users_list)
+
+def send_reminder_on_slack(users_list):
+    slack_token = frappe.db.get_single_value(
+            "Slack Integration Settings", 'ped_token')
+    if slack_token and len(slack_token)>0:
+        response_data=[{
+            "type": "header",
+            "text": {"type": "plain_text", "text": "* This is a gentle reminder to take action on proposals that are pending your approval or rejection. *"}
+        }]
+        for email in users_list:
+            user_id = get_user_id_by_email(email, slack_token)            
+            if user_id:
+                
+                payload ={
+                    "type": 'header',
+                    "blocks": response_data
+                }
+                payload["channel"] = user_id
+                payload = payload.copy()
+                url = "https://slack.com/api/chat.postMessage"
+                headers = {
+                    "Authorization": f"Bearer {slack_token}",
+                    "Content-Type": "application/json",
+                }
+                response = requests.post(url, headers=headers, json=payload)
 
